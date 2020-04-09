985ad46ec5552b18e4a58e0b6cac31b9a2d4d4b4
Rachael A. Concessio <RachaelConcessio@gmail.com>
2020-04-09 14:53:38 -0400

    First Commit

---
+++ build/precache-manifest.f360524cd26d2b95d6cf952e778dbba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2f8a846cb508265d4289c72d145bce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d0d8752007d2c9bd15a",
+    "url": "/static/css/main.969e7c60.chunk.css"
+  },
+  {
+    "revision": "208d2da7967f17c2e71c",
+    "url": "/static/js/2.4dcc5899.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4dcc5899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0d8752007d2c9bd15a",
+    "url": "/static/js/main.366582cc.chunk.js"
+  },
+  {
+    "revision": "ad73402b8c11f3ab3b3d",
+    "url": "/static/js/runtime-main.51a97069.js"
+  }
+]);
